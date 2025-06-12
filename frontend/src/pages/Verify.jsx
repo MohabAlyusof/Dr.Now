1b3b97a05c67538a30a8da0201846d1f7be4b71e
Mohab Alyusof <Mohabalusef-1997@hotmail.com>
2025-06-12 16:47:07 +0200

    fix Verify.jsx

--- frontend/src/pages/Verify.jsx
+++ frontend/src/pages/Verify.jsx
@@ -36,10 +36,11 @@ const Verify = () => {
   };
 
   useEffect(() => {
-    if ((token, appointmentId, success)) {
-      verifyStripe();
-    }
-  }, [token]);
+  if (token && appointmentId && success) {
+    verifyStripe();
+  }
+}, [token, appointmentId, success]);
+
 
   return (
     <div className="min-h-[60vh] flex items-center justify-center">
