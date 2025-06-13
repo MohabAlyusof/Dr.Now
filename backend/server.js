08b563d06bdd5dc2c55ac3d3557e0818f96877f9
Mohab Alyusof <Mohabalusef-1997@hotmail.com>
2025-06-13 17:49:47 +0200

    add new change

--- backend/server.js
+++ backend/server.js
@@ -17,7 +17,7 @@ connectCloudinary();
 // middlewares
 app.use(express.json());
 app.use(cors({
-  origin: process.env.CLIENT_URL || "http://localhost:7777",
+  origin: process.env.CLIENT_URL,
   credentials: true,
 }));
 app.use("/api/agora", agoraTokenRoute);
